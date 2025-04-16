bd20169f3d6514dc5be02a7696bf83c60cf056b2
anton <ashtanyuk@gmail.com>
2025-04-16 14:10:15 +0300

    Initial commit

---
+++ ProjCharGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generator
+{
+    class CharGenerator 
+    {
+        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
+        private char[] data;
+        private int size;
+        private Random random = new Random();
+        public CharGenerator() 
+        {
+           size = syms.Length;
+           data = syms.ToCharArray(); 
+        }
+        public char getSym() 
+        {
+           return data[random.Next(0, size)]; 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CharGenerator gen = new CharGenerator();
+            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
+            for(int i = 0; i < 1000; i++) 
+            {
+               char ch = gen.getSym(); 
+               if (stat.ContainsKey(ch))
+                  stat[ch]++;
+               else
+                  stat.Add(ch, 1); Console.Write(ch);
+            }
+            Console.Write('\n');
+            foreach (KeyValuePair<char, int> entry in stat) 
+            {
+                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+            }
+            
+        }
+    }
+}
+
